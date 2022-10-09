debead75c74398e5d5e822a7276b24c783c73217
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-10-09 14:44:08 +0200

    UsersPage done

---
+++ src/components/UserItem.tsx
@@ -0,0 +1,18 @@
+import { Card } from '@mantine/core';
+
+import { User } from '../types/User';
+
+type UserItemProps = {
+  user: User;
+};
+
+export default function UserItem({ user }: UserItemProps) {
+  return (
+    <Card p={5} shadow="sm" withBorder>
+      <Card.Section p={5}>Name: {user.name}</Card.Section>
+      <Card.Section p={5}>Email: {user.email}</Card.Section>
+      <Card.Section p={5}>Status: {user.status}</Card.Section>
+      <Card.Section p={5}>Gender: {user.gender}</Card.Section>
+    </Card>
+  );
+}
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,36 +1,55 @@
 import { Container, Grid } from '@mantine/core';
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useInfiniteQuery } from 'react-query';
 
+import UserItem from '../components/UserItem';
 import { User } from '../types/User';
 
-const getUsers = async () => {
-  const res = await axios.get('/public/v1/users');
+const getUsers = async (pageNumber: number) => {
+  const res = await axios.get('/public/v1/users?page=' + pageNumber);
   return res.data;
 };
 
 function UsersPage() {
-  const { isLoading, data, status } = useQuery('users', getUsers);
-  const { meta, data: userData } = data;
+  const [page, setPage] = React.useState(1);
+  const { data, status, fetchNextPage } = useInfiniteQuery(
+    'users',
+    ({ pageParam = page }) => getUsers(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage + 1,
+      getPreviousPageParam: (firstPage) => firstPage - 1,
+    },
+  );
 
-  if (isLoading) return <Container>Loading...</Container>;
+  if (status === 'loading') return <Container>Loading...</Container>;
+  if (status === 'error') return <Container>Error</Container>;
+
+  const getNextUsers = async () => {
+    setPage((prev) => prev + 1);
+    await fetchNextPage({ pageParam: page });
+  };
 
   return (
     <Container mt={20}>
       <h4>Users</h4>
-      <Grid>
-        {status === 'success' &&
-          userData.map((user: User) => (
-            <Grid.Col span={5} key={user.id}>
-              <Container>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-                <p>{user.gender}</p>
-                <p>{user.status}</p>
-              </Container>
-            </Grid.Col>
-          ))}
-      </Grid>
+      <InfiniteScroll
+        next={getNextUsers}
+        loader={<Container mt={20}>Loading...</Container>}
+        hasMore={true}
+        dataLength={data?.pages.concat(data?.pages ?? []).length || [].length}
+      >
+        <Grid>
+          {data?.pages.map((page) =>
+            page.data.map((user: User) => (
+              <Grid.Col span={4} key={user.id}>
+                <UserItem user={user} />
+              </Grid.Col>
+            )),
+          )}
+        </Grid>
+      </InfiniteScroll>
     </Container>
   );
 }
